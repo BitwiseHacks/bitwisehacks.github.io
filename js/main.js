941cc0857d179f00399c4ef0062a81f0940e8a6a
Sasha Varlamov <sasha@sashavarlamov.com>
2016-03-13 17:02:17 +0800

    Functional contact form, and scrolling fancybox

--- js/main.js
+++ js/main.js
@@ -168,6 +168,10 @@ BWH.fancyBox = function(){
 				beforeShow: function () {
 					this.title = $(this.element).attr('title');
 					this.title = '<h4>' + this.title + '</h4>' + '<p>' + $(this.element).parent().find('img').attr('alt') + '</p>';
+					$("body").css({'overflow-y':'hidden'});
+				},
+				afterClose: function() {
+					$("body").css({'overflow-y':'visible'});
 				},
 				helpers : {
 					title : { type: 'inside' },
@@ -189,11 +193,16 @@ BWH.fancyBox = function(){
    Contact Form
 ================================================== */
 
+function isValidEmailAddress(emailAddress) {
+			var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+			return pattern.test(emailAddress);
+		};
+
 BWH.contactForm = function(){
 	$("#contact-submit").on('click',function() {
 		$contact_form = $('#contact-form');
 		
-		var fields = $contact_form.serialize();
+		/*var fields = $contact_form.serialize();
 		
 		$.ajax({
 			type: "POST",
@@ -210,6 +219,33 @@ BWH.contactForm = function(){
 				$('#response').empty().html(response.html);
 			}
 		});
+		return false;*/
+		var c_name = $('#c_name').val();
+		var c_email = $('#c_email').val();
+		var c_message = $('#c_message ').val();
+		var response = $('#c_response');
+		if (c_name == '') {
+			response.html('Please enter your name, and try again').fadeIn(500);
+		} else if (c_email == '') {
+			response.html('Please enter your email, and try again').fadeIn(500);
+		} else if (c_message == '') {
+			response.html('Please enter your message, and try again').fadeIn(500);
+		} else if (!isValidEmailAddress(c_email)) {
+			response.html('Please enter a valid email address, and try again').fadeIn(500);
+		}
+		else {
+			var pdat = "";
+			pdat += "field-0=" + encodeURIComponent(c_name);
+			pdat += '&field-1=' + encodeURIComponent(c_email);
+			pdat += '&field-2=' + encodeURIComponent(c_message);
+			var xhr = new XMLHttpRequest();
+			xhr.open('POST', 'http://former.io/f/1rNe32ufN119RW8n?' + pdat, true);
+			xhr.onreadystatechange = function(){
+				$('#contact-form p').fadeOut(500);
+				response.html("Your message has been sent, and we'll get back to you ASAP!").fadeIn(500);
+			}
+			xhr.send();  
+		}
 		return false;
 	});
 }
