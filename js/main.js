3ca914af1332d24be84b2d946bee3972fd457490
Sasha Varlamov <sasha@sashavarlamov.com>
2016-03-13 09:25:07 +0800

    Remove loading spinner

--- js/main.js
+++ js/main.js
@@ -438,7 +438,7 @@ $(document).ready(function(){
 	]);
 	
 	// Preload the page with jPreLoader
-	$('body').jpreLoader({
+	/*$('body').jpreLoader({
 		splashID: "#jSplash",
 		showSplash: true,
 		showPercentage: true,
@@ -446,7 +446,7 @@ $(document).ready(function(){
 		splashFunction: function() {
 			$('#circle').delay(250).animate({'opacity' : 1}, 500, 'linear');
 		}
-	});
+	});*/
 	
 	BWH.nav();
 	BWH.mobileNav();
